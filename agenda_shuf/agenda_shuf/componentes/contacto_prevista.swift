de9f35115ff5afc81d0bfa15e495c9af7c00d1ab
alumno <alumno@pc-candelap.uacj.mx>
2025-02-26 10:14:02 -0700

    [AGREGA] componente para la vista de contacto prevista:(

---
+++ agenda_shuf/agenda_shuf/componentes/contacto_prevista.swift
@@ -0,0 +1,23 @@
+//
+//  contacto_prevista.swift
+//  agenda_shuf
+//
+//  Created by alumno on 26/02/25.
+//
+
+import SwiftUI
+
+struct contacto_prevista: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "globe")
+            Text("Miau")
+            
+        }
+
+    }
+}
+
+#Preview {
+    contacto_prevista()
+}
