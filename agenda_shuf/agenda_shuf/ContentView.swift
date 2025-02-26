65686fe85ab3e8e2df51df2734e6bdcb5d75589c
alumno <alumno@pc-candelap.uacj.mx>
2025-02-26 09:46:12 -0700

    [INICIALIZA] el proyecto xcode:(

---
+++ agenda_shuf/agenda_shuf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  agenda_shuf
+//
+//  Created by alumno on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ agenda_shuf/agenda_shuf/agenda_shufApp.swift
@@ -0,0 +1,17 @@
+//
+//  agenda_shufApp.swift
+//  agenda_shuf
+//
+//  Created by alumno on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct agenda_shufApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PantallaAgenda()
+        }
+    }
+}
