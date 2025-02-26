30e18d7478851dc54c94cdd2174f0335d8e03afd
alumno <alumno@pc-candelap.uacj.mx>
2025-02-26 10:45:50 -0700

    [AGREGA] un modelo de contactos pa guardar info

---
+++ agenda_shuf/agenda_shuf/modelos/contacto_agenda.swift
@@ -0,0 +1,27 @@
+//
+//  contacto_agenda.swift
+//  agenda_shuf
+//
+//  Created by alumno on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContactoAgenda: Identifiable {
+    var nombre: String
+    var telefono: String
+    
+    var id: String { nombre }
+}
+
+/*
+var nombre = "Pepito Veraz"
+
+var _nombre: String
+var nombre: set{
+    let nombre_validado = filtrar_palabras(value):
+            _nombre = nombre_validado
+} get {
+    return _nombre.split(" ")[0]
+}
+*/
